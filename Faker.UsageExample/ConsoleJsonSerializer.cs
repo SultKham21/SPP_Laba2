c1edab372979b56d5a9985960bda008c40492651
SultKham21 <khamroevsulton2001@gmail.com>
2021-10-25 20:11:08 +0300

    Faker

---
+++ Faker.UsageExample/ConsoleJsonSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace Faker.UsageExample
+{
+    public static class ConsoleJsonSerializer
+    {
+        public static void Serialize<T>(T toSerialize)
+        {
+            using (var jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(Console.OpenStandardOutput(), Encoding.UTF8, ownsStream: true, indent: true))
+            {
+              new DataContractJsonSerializer(typeof(T)
+                   ,new DataContractJsonSerializerSettings
+              {
+                 DateTimeFormat =  new DateTimeFormat("yyyy-MM-dd' Time 'HH:mm:ssZ")//("yyyy-MM-dd'T'HH:mm:ssZ")
+              }
+            ).WriteObject(jsonWriter, toSerialize);
+              //new DataContractJsonSerializer(typeof(T)).WriteObject(jsonWriter, toSerialize);
+            }
+        }
+    }
+}
---
+++ Faker.UsageExample/ExampleClassProperties.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Faker.UsageExample
+{
+    public class ExampleClassProperties
+    {       
+        [DataMember]
+        public DateTime dateTimeField;
+        [DataMember]
+        public object objectField;
+        [DataMember]
+        public char publicStringField;
+        [DataMember]
+        public bool publicBoolField;
+        [DataMember]
+        protected bool nonPublicBoolField;
+
+        [DataMember]
+        public int PublicIntSetter
+        { get; set; }
+
+        [DataMember]
+        public int NonPublicIntSetter
+        { get; protected set; }
+
+        [DataMember]
+        private readonly int nonPublicIntField;
+
+        [DataMember]
+        public bool[] publicList;
+        
+       [DataMember]
+         public List<int> publicArray;
+        
+        //[DataMember]
+        //public List<List<char>> publicListInList;
+
+        [DataMember]
+        public NewClass nestedObject;
+
+        [DataMember]
+        public int CustomGeneratorCheckProperty
+        { get; set; }
+
+        public ExampleClassProperties()
+        {
+        }
+    }
+}
